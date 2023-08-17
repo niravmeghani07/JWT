69c87cd01d5092c2cbc97797fd9180a7764e0cbd
Nirav Meghani <nc90762@gmail.com>
2023-08-17 12:23:03 +0530

    JWT added

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const port = 1000;
+const jwt = require('jsonwebtoken');
+app.use(express.json());
+
+const data = [
+    {
+        username: 'Nirav',
+        title: 'Post 1'
+    },
+    {
+    username: 'Kyle',
+    title: 'Post 2'
+    }
+]
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.get('/posts',authenticateToken,(req,res)=>{
+    res.json(data.filter(item => item.username === req.user.name));
+})
+
+app.post('/login',(req,res)=>{
+    const username= req.body.username;
+    const user = { name: username};
+    const access_token = jwt.sign(user, process.env.ACCESS_SECRET_KEY);
+    res.json({access_Token: access_token})
+})
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err,user)=>
+    {
+        if(err) return res.sendStatus(403)
+        req.user = user;
+        next();
+    })
+
+}
+
+app.listen(port,()=>{
+    console.log('Connected to server');
+});
